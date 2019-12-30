d7594fa6600e2812e5b9c8d8bc32090c42f14125
ujjwol <ujjwolneupane75@gmail.com>
2019-12-30 21:57:39 +0545

    first commit

---
+++ js/external.js
@@ -0,0 +1,12 @@
+
+ 
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[767,1],
+        pagination: true,
+        autoPlay:true
+    });
+});
